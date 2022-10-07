c2a4ebcce6cf649d82c21b8599df6cb9876a527a
Maarten Geerse <maarten.geerse@bettyblocks.com>
2022-10-07 14:21:19 -0400

    feat: update testcase

--- test/fetch-json-variable.test.js
+++ test/fetch-json-variable.test.js
@@ -12,4 +12,29 @@ test('fetchJsonVariable 1.0', async () => {
     keyName: 'wrongKey',
   });
   assert(outputTwo, { output: '' });
+
+  const outputThree = await $app['fetchJsonVariable 1.0']({
+    jsonInput: {
+      access_token: 'x6uPha4TjHWnjNZedSzICJ2wC3BMBM1u',
+      refresh_token_expires_in: '31536000',
+      expires_in: '2678400',
+      refresh_token: 'BNDHbjewLIDZ3z3gbUUTrnOeg8EgLOxu',
+      token_type: 'bearer',
+      useremail: 'marcel.korporaal@bettyblocks.com',
+    },
+    keyName: 'access_token',
+  });
+  assert(outputThree, { output: 'x6uPha4TjHWnjNZedSzICJ2wC3BMBM1u' });
+  const outputFour = await $app['fetchJsonVariable 1.0']({
+    jsonInput: {
+      access_token: 'x6uPha4TjHWnjNZedSzICJ2wC3BMBM1u',
+      refresh_token_expires_in: '31536000',
+      expires_in: '2678400',
+      refresh_token: 'BNDHbjewLIDZ3z3gbUUTrnOeg8EgLOxu',
+      token_type: 'bearer',
+      useremail: 'marcel.korporaal@bettyblocks.com',
+    },
+    keyName: 'refresh_token',
+  });
+  assert(outputFour, { output: 'BNDHbjewLIDZ3z3gbUUTrnOeg8EgLOxu' });
 });
