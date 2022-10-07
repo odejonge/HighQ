5d434e21fdd9ffdad596eadde914ecbc2e5fdca6
Maarten Geerse <maarten.geerse@bettyblocks.com>
2022-10-07 11:11:20 -0400

    feat: better test for get-limit-offset

--- test/get-limit-offset.test.js
+++ test/get-limit-offset.test.js
@@ -1,53 +1,53 @@
 test('getLimitOffset 1.0', async () => {
   const output = await $app['getLimitOffset 1.0']({
     params: {
-      where: {
-        _or: [
-          {
-            _and: [
-              {
-                email: {
-                  eq: 'maarten.geerse@bettyblocks.com',
-                },
-              },
-            ],
-          },
-          {
-            _and: [
-              {
-                goesby: {
-                  eq: null,
-                },
-              },
-            ],
-          },
-          {
-            _and: [
-              {
-                id: {
-                  lt: 2,
-                },
-              },
-            ],
-          },
-          {
-            _and: [
-              {
-                orgid: {
-                  neq: null,
-                },
-              },
-              {
-                orgaddress: {
-                  ends_with: '5',
-                },
-              },
-            ],
-          },
-        ],
+      auth_info: {
+        auth_profile: null,
+        raw_jwt: null,
+        user_id: null,
       },
+      conditions: [],
+      data_source_info: {
+        kind: 'OpenAPIv2',
+        model: {
+          meta_info: {
+            methods: [
+              {
+                endpoint: '/{version}/sites/people',
+                method: 'get',
+              },
+            ],
+            schema_type: '#/definitions/PersonDBO',
+          },
+        },
+        source: {
+          api_key: null,
+          api_key_required: true,
+          host: 'https://marcel.bettywebblocks.com/ghqswagger',
+        },
+      },
+      input: null,
+      select: [
+        'avatar',
+        'httpavatar',
+        'title',
+        'goesby',
+        'orgname',
+        'orgid',
+        'userid',
+        'userlink',
+        'httplink',
+        'id',
+        'firstname',
+        'lastname',
+        'orgaddress',
+        'email',
+      ],
       skip: 0,
+      sort: null,
       take: 5,
+      total_count: true,
+      variables: [],
     },
   });
   assert(output, {
