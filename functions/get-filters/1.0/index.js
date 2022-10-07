4c766bd9f116fd75645df23313008cf318929cec
Maarten Geerse <maarten.geerse@bettyblocks.com>
2022-10-07 08:54:34 -0400

    feat: remake getFilters function

--- functions/get-filters/1.0/index.js
+++ functions/get-filters/1.0/index.js
@@ -1,5 +1,25 @@
-const getFilters = async () => {
+const getFilters = async ({ params: { where } }) => {
+  let result = {};
 
-}
+  let propName = '';
+  const getFilters = (condition) => {
+    for (const [key, value] of Object.entries(condition)) {
+      if (typeof value !== 'object' || value === null) {
+        result[propName] = value;
+      } else if (
+        (typeof value === 'object' || Array.isArray(value)) &&
+        value !== null
+      ) {
+        propName = key;
+        getFilters(condition[key]);
+      }
+    }
+    return result;
+  };
+
+  return {
+    output: getFilters(where),
+  };
+};
 
 export default getFilters;
