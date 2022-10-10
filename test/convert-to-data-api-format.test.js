ba70811d2b19a0da0130a78345e407203888dcdd
Maarten Geerse <maarten.geerse@bettyblocks.com>
2022-10-10 16:06:33 -0400

    chore: update convert test

--- test/convert-to-data-api-format.test.js
+++ test/convert-to-data-api-format.test.js
@@ -1,12 +1,14 @@
 test('convertToDataApiFormat 1.0', async () => {
   const output = await $app['convertToDataApiFormat 1.0']({
     input: JSON.stringify(peoplesJson),
+    fields:
+      '["userid","email","httpavatar","avatar","userlink","httplink","title","firstname","lastname","goesby","orgname","orgid","orgaddress","id"]',
     path: 'person',
   });
   assert(output, {
     output: {
       totalCount: 5,
-      results: peoplesJson['person'],
+      results: convertedResponse,
     },
   });
 });
