7ca6210a97e452e30c59bbe9d3d1e563f0c1067f
Maarten Geerse <maarten.geerse@bettyblocks.com>
2023-02-03 20:54:44 -0500

    feat: count

--- functions/count/1.0/index.js
+++ functions/count/1.0/index.js
@@ -1,13 +1,7 @@
 const count = async ({ countable }) => {
-  try {
-    JSON.parse(countable);
-  } catch (e) {
-    throw new Error(e);
-  } finally {
-    return {
-      as: countable.length,
-    };
-  }
+  return {
+    as: countable.length,
+  };
 };
 
 export default count;
