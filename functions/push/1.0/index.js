02ca18676a4fef36ae93ae63aab3cd463041a852
Maarten Geerse <maarten.geerse@bettyblocks.com>
2023-02-07 10:39:00 -0500

    feat: remove console logs

--- functions/push/1.0/index.js
+++ functions/push/1.0/index.js
@@ -1,11 +1,10 @@
 const push = async ({ arrayInput = new Array(), element }) => {
-  console.log(JSON.stringify(arrayInput));
   if (!Array.isArray(arrayInput)) {
     throw new Error("The input array isn't an array.");
   }
-  console.log(element);
+
   arrayInput.push(element);
-  console.log(arrayInput);
+
   return {
     as: arrayInput,
   };
